e514b41488779e0454be22ed94ee7d8bc298e81e
Gaurav Sarkar <60139335+Gaurav7888@users.noreply.github.com>
2023-01-11 03:13:06 +0530

    Create spending_analysis.py

---
+++ pages/spending_analysis.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.sidebar.markdown(" :)( ")
+
+st.title("Spending Analysis")
+
+st.image("screenshots/Screenshot from 2023-01-11 03-01-53.png")
+
+st.image("screenshots/Screenshot from 2023-01-11 03-01-43.png")
+
+st.image("screenshots/Screenshot from 2023-01-11 03-01-33.png")
+
+st.image("screenshots/Screenshot from 2023-01-11 03-01-18.png")
+
+st.image("screenshots/Screenshot from 2023-01-11 03-01-07.png")
+
+st.image("screenshots/Screenshot from 2023-01-11 03-00-54.png")
+
+st.image("screenshots/Screenshot from 2023-01-11 03-00-37.png")
+
+st.image("screenshots/Screenshot from 2023-01-11 03-00-19.png")
