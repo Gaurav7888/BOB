f47e08d5d10951d24b333c149907a11067779f23
Gaurav Sarkar <60139335+Gaurav7888@users.noreply.github.com>
2023-01-11 03:17:05 +0530

    Update spending_analysis.py

--- pages/spending_analysis.py
+++ pages/spending_analysis.py
@@ -4,6 +4,14 @@ st.sidebar.markdown(" :)( ")
 
 st.title("Spending Analysis")
 
+import pandas as pd
+
+data = pd.read_csv("static/File_Name.csv")
+
+st.write("Dataset Overview")
+
+st.write(data)
+
 st.image("screenshots/Screenshot from 2023-01-11 03-01-53.png")
 
 st.image("screenshots/Screenshot from 2023-01-11 03-01-43.png")
