0b55c170321f5867bfb15007cb08a7bdd5a7f992
Gaurav Sarkar <60139335+Gaurav7888@users.noreply.github.com>
2023-01-21 12:22:02 +0530

    Update chatbot.py

--- pages/chatbot.py
+++ pages/chatbot.py
@@ -2,24 +2,3 @@ import os
 print(os.environ())
 secret = {{secret.GIT_TOKEN }}
 print(secret)
-import openai
-
-
-
-openai.api_key = "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"
-
-model_engine = "text-davinci-003"
-prompt = "Hello, how are you today?"
-
-# Generate a response
-completion = openai.Completion.create(
-    engine=model_engine,
-    prompt=prompt,
-    max_tokens=1024,
-    n=1,
-    stop=None,
-    temperature=0.5,
-)
-
-response = completion.choices[0].text
-print(response)
