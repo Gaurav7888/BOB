66bf8edc06869f2939299d41de99d27775e7676d
Gaurav Sarkar <60139335+Gaurav7888@users.noreply.github.com>
2023-01-21 11:28:29 +0530

    Update Fraud_Detection.py

--- pages/Fraud_Detection.py
+++ pages/Fraud_Detection.py
@@ -1,5 +1,15 @@
 import streamlit as st
 import pandas as pd
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from random import seed,sample
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import classification_report,confusion_matrix,accuracy_score, roc_curve, auc,precision_score
+from sklearn.ensemble import RandomForestClassifier
 
 st.title("Classifying Fraudulent and Valid Transactions")
 
@@ -32,21 +42,14 @@ isFraud - boolean/binary - determines if transaction is fraudulent (encoded as 1
 isFlaggedFraud - boolean/binary - determines if transaction is flagged as fraudulent (encoded as 1) or not flagged at all (encoded as 0). An observation is flagged if the transaction is fraudulent and it involved a transfer of over 200,000 in the local currency.""")
 
 
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-from random import seed,sample
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import classification_report,confusion_matrix,accuracy_score, roc_curve, auc,precision_score
-from sklearn.ensemble import RandomForestClassifier
+
 
 
 data.pop('Unnamed: 0')
 
 correlation=data.corr()
+
+st.write("Correlation Value for isFraud")
 st.write(correlation['isFraud'])
 
 data["type"] = data["type"].map({"CASH_OUT": 1, "PAYMENT": 2, "CASH_IN": 3, "TRANSFER": 4,"DEBIT": 5})
